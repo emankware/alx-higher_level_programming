5ed93bc90cad59beb03f55dd065526c9ab1a7e13
emankware <lucymankware@gmail.com>
2023-11-02 03:38:35 -0700

    updates

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,5 @@
--- inserts a new row in the table first_table
--- new row attributes are id = 89, name = Best School
+-- displays the number of records with id = 89
 
-INSERT INTO first_table( id, name)
-VALUES ( 89, 'Best School');
+SELECT COUNT(*)
+FROM first_table
+WHERE id = 89;
